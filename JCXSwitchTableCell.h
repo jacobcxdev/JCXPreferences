c33d378134206032317249ca372ad2dfe8b28742
JacobCXDev <jacobcxdev@gmail.com>
2020-03-18 20:22:49 +0000

    Added Switch cell

---
+++ JCXSwitchTableCell.h
@@ -0,0 +1,5 @@
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSSwitchTableCell.h>
+
+@interface JCXSwitchTableCell : PSSwitchTableCell
+@end
---
+++ JCXSwitchTableCell.m
@@ -0,0 +1,10 @@
+#import "JCXSwitchTableCell.h"
+
+@implementation JCXSwitchTableCell
+- (void)controlChanged:(UIControl *)control {
+    NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:[NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", _specifier.properties[@"defaults"]]];
+    [plist setObject:[self controlValue] forKey:_specifier.properties[@"key"]];
+    [plist writeToFile:[NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", _specifier.properties[@"defaults"]] atomically:1];
+	[super controlChanged:control];
+}
+@end
