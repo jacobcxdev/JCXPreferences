0e7a57d7a3a7f9d8f7a07ce3679bcb24d8cc5afd
JacobCXDev <jacobcxdev@gmail.com>
2020-04-05 04:10:09 +0100

    Bypass CFPreferences and use FS

--- JCXRootListController.m
+++ JCXRootListController.m
@@ -1,4 +1,4 @@
-#include "JCXRootListController.h"
+#import "JCXRootListController.h"
 
 @implementation JCXRootListController
 - (instancetype)init {
@@ -8,5 +8,22 @@
 - (NSArray *)specifiers {
     if (!_specifiers) _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     return _specifiers;
+}
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+    NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+    NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+	return (plist[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+}
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+    NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+    NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+	plist[specifier.properties[@"key"]] = value;
+	[plist writeToFile:path atomically:true];
+	CFStringRef notificationName = (__bridge CFStringRef)specifier.properties[@"PostNotification"];
+	if (notificationName)
+		CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), notificationName, NULL, NULL, true);
+}
+- (void)pushViewController {
+    [self.navigationController pushViewController:[UIViewController new] animated:true];
 }
 @end
